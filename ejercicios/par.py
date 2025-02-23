4c6200e86550d30f94702f0aab1546e809f69d12
Jorge Rivera <jorge.rivera@ssde.com.mx>
2025-02-22 23:18:54 -0600

    UDG-PE-02. Adicion de nuevos programas. 19022025

---
+++ ejercicios/par.py
@@ -0,0 +1,12 @@
+"""
+    Dado un numero determinar si es par o impar
+"""
+
+n = int(input("Ingresa un numero: "))
+
+if n % 2 == 0:
+    print("El numero {} es par".format(n))
+else:
+    print("El numero {} es impar".format(n))    
+
+print("Fin del programa")
--- ejercicios/pares.py
+++ ejercicios/pares.py
@@ -3,11 +3,18 @@
     Determina si un numero es par o impar
 '''
 
+
 n = int(input("Ingresa un numero: "))
 
-if n % 2 == 0:
-    print("El numero {} es par".format(n))
-else:
-    print("El numero {} es impar".format(n))    
+print("El resultado del programa utilizando condicional: \n")
 
-print("Fin del programa")   
+if n % 2 == 0:
+    print("El numero {} es par\n".format(n))
+else:
+    print("El numero {} es impar\n".format(n))    
+
+print("El resultado del programa utilizando operdor ternario: \n")
+
+print("El numero {} es par\n".format(n) if n % 2 == 0 else "El numero {} es impar\n".format(n))
+
+print("Fin del programa")
